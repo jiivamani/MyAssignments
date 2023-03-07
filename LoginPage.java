12ef4815c8c63dfcb3de65d14d4aee5a3f1a1637
jiivamani <127200745+jiivamani@users.noreply.github.com>
2023-03-07 22:31:18 +0530

    Add files via upload
    login page url set up has completed

---
+++ LoginPage.java
@@ -0,0 +1,13 @@
+package SeleniumSampleCode;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+	}
+}
+ 
